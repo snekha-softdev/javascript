8ce19d92c2a41a38fcb4717b1297c34a070b7f76
snekha-softdev <snekhasoftdev@gmail.com>
2025-12-19 21:49:29 +0530

    array method is pushed by #snekha-softdev

---
+++ 08_arraymethods.js
@@ -0,0 +1,54 @@
+// push
+let arr=[1,2,3,4,5,6];
+arr.push(10);
+console.log(arr);
+// pop
+let arr1=[10,20,30,40];
+arr1.pop();
+console.log(arr1)
+// unshift
+let arr2=[10,29,40,43];
+arr2.unshift(1);
+console.log(arr2);
+// shift
+let arr8 = [1,2,3,4,5,6];
+arr8.shift(10);
+console.log(arr8);
+// splice
+let arr3 = [1, 2, 3, 4];
+// remove
+arr3.splice(1, 3);
+console.log(arr3);
+// add
+let arr4= [10,20,30,40,50];
+arr4.splice(5,60,70,80);
+console.log(arr4);
+// replace
+let arr5= [1,2,3,4,5,6,7,8,9];
+arr5.splice(5,3,60,70,80);
+console.log(arr5);
+// sort
+let arr12 = [ 10,20,5,0,];
+arr12.sort();
+console.log(arr12);
+// reverse
+let arr6 = [1, 2, 3];
+arr6.reverse();
+
+console.log(arr6); 
+// fill
+let arr7 = [1, 2, 3];
+arr7.fill(1);
+
+console.log(arr7); 
+// copywithin
+let arr13 = [10,20,30,40,50,60,70,80,90];
+arr13.copyWithin(3,1,7);
+console.log(arr13);
+
+
+
+
+
+
+
