9a3b9b185d02a8c9f05bee28f0fde2d584738b3b
snekha-softdev <snekhasoftdev@gmail.com>
2025-12-26 21:26:22 +0530

    opeartors is pushed by #snekha-softdev

---
+++ 04_operations.js
@@ -0,0 +1,30 @@
+
+// arithmitic operations
+const a = 10;
+      const b = 5;
+  
+      console.log(a + b); 
+      console.log(a - b); 
+      console.log(a * b); 
+      console.log(a / b); 
+      console.log(a % b); 
+      console.log(a ** b);
+    //   comparison operators
+    const a1 = 10;
+      const b1 = 5;
+  
+      console.log(a1 > b1); 
+      console.log(a1 < b1);
+      console.log(a1 >= b1); 
+      console.log(a1 <= b1); 
+      console.log(a1 === b1); 
+      console.log(a1 !== b1);
+    //   lodical operators
+     const a11 = false;
+      const b11 = false;
+  console.log("AND operation")
+      console.log(a11 && b11); // Logical AND: false
+      console.log("OR operation")
+      console.log(a11 || b11); // Logical OR: true
+console.log("Not operator")
+      console.log(!a11);
