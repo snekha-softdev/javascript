cf0232855b7e7875a0b2f0948def842e238d6ce6
snekha-softdev <snekhasoftdev@gmail.com>
2026-01-01 21:07:32 +0530

    conditional statement is pushed by #snekha-softdev

---
+++ 06_conditionstatements.js
@@ -0,0 +1,61 @@
+// if statement
+let age = 18;
+
+if (age >= 18) {
+  console.log("You are eligible to vote");
+}
+// if else statement
+let marks = 40;
+if (marks >= 50) {
+  console.log("Pass");
+} else {
+  console.log("Fail");
+}
+// switch statement
+let day = 3;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  default:
+    console.log("Invalid day");
+}
+// Teranary operator
+let ages = 16;
+
+let result = (ages >= 18) ? "Adult" : "Minor";
+console.log(result);
+ let age11="snekha";
+ console.log(age11);
+ 
+//  else statement
+let age111 = 15;
+if (age111>=45){
+    console.log("your are termimated");
+}
+else {
+    console.log("welldone Yor are stay tha comapny")
+}
+let gender="female";
+if(gender="female"){
+console.log("what is your age");
+}
+else if (gender="trans"){
+    console.log("what is your name")
+
+}
+else{
+    Console.log("you are not allowed")
+}
+// teranary operator
+let final = age>18? "you are elligable to vote": "you are not elligable to vote";
+console.log(final)
+
+
