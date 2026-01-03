89fb1f58e72a4ed4a6c77e9c22618111d04b77ba
snekha-softdev <snekhasoftdev@gmail.com>
2026-01-03 19:13:10 +0530

    date is pushed by #snekha-softdev

---
+++ 15_dates.js
@@ -0,0 +1,19 @@
+// create a date 
+let now = new Date();
+console.log(now);
+// get date values
+let d = new Date();
+
+console.log(d.getFullYear()); 
+console.log(d.getMonth());     
+console.log(d.getDate());      
+console.log(d.getDay()); 
+// set date and times
+let d1 = new Date();
+
+d1.setFullYear(2027);
+d1.setMonth(5);
+d1.setDate(15);
+
+console.log(d1);
+
